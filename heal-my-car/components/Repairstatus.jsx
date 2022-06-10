8a2af6cded4a9013c3967e180a96b23f6656f6b0
Bartek Nowakowski <nowakowskib@gmail.com>
2022-06-10 17:23:42 +0200

    WIP-Table with RepairStatus

---
+++ heal-my-car/components/Repairstatus.jsx
@@ -0,0 +1,71 @@
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import { useState, useEffect } from 'react';
+import { db } from "../src/firebase";
+import { collection, getDocs, query, where } from "firebase/firestore";
+import { Collapse } from '@mui/material';
+
+const Repairstatus = () => {
+  const [repairs, setRepairs] = useState([]);
+  const [openRowIndex, setOpenRowIndex] = useState();
+
+  useEffect(() => {
+    getRepairs();
+  }, []);
+
+  const getRepairs = () => {
+    const repairsCollection = collection(db, "repairs");
+    const pendingRepairsQuery = query(repairsCollection, where("isDone", "==", false));
+
+    getDocs(pendingRepairsQuery).then((QuerySnapshot) => {
+      setRepairs(QuerySnapshot.docs.map((doc) => doc.data()));
+    });
+  };
+
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 450 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell><b>Brand</b></TableCell>
+            <TableCell><b>VIN</b></TableCell>
+            <TableCell><b>Status</b></TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {repairs.map((row, index) => (
+            <>
+            <TableRow
+              key={row.carVin}
+              onClick={() => setOpenRowIndex(index)}
+              sx={{ '&:last-child td, &:last-child th': { border: 50 } }}
+            >
+              <TableCell>
+                {row.carBrand}
+              </TableCell>
+              <TableCell>
+                {row.carVin}
+              </TableCell>
+              <TableCell>
+                {row.isDone ? "Done" : "Pending"}
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <Collapse in={index === openRowIndex}>
+              {row.clientId}
+              </Collapse>
+            </TableRow>
+            </>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+};
+
+export default Repairstatus;
--- heal-my-car/src/App.jsx
+++ heal-my-car/src/App.jsx
@@ -1,22 +1,7 @@
-import { db } from "./firebase";
-import { collection, getDocs } from "firebase/firestore";
-import { useEffect } from "react";
+import Repairstatus from "../components/Repairstatus";
 
 function App() {
-  useEffect(() => {
-    getRepairs();
-  });
-
-  const getRepairs = () => {
-    const collection0 = collection(db, "repairs");
-    getDocs(collection0).then((QuerySnapshot) => {
-      QuerySnapshot.docs.forEach((doc) => {
-        console.log("zlecenie naprawy", doc.data());
-      });
-    });
-  };
-
-  return <h1 style={{ textAlign: "center" }}>Firebase configuration test</h1>;
+  return <Repairstatus />;
 }
 
 export default App;
