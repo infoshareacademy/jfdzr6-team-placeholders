52f084f8358f08d57c21e096724f9751c3fe0d37
Daniel <jackiewicz.daniel@gmail.com>
2022-06-25 19:12:36 +0200

    aaa

--- heal-my-car/components/admin-panel/clientsData/RepairsInProgress.jsx
+++ heal-my-car/components/admin-panel/clientsData/RepairsInProgress.jsx
@@ -1,55 +1,54 @@
-import { collection, getDocs, where, query, getDoc } from 'firebase/firestore'
-import React, { useEffect, useState } from 'react'
-import { db } from '../../../src/firebase'
-import { MapClients } from './MapClients'
-import { MapRepairsInProgress } from './MapRepairsInProgress'
+import { collection, getDocs, where, query, getDoc } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
+import { db } from "../../../src/firebase";
+import { MapClients } from "./MapClients";
+import { MapRepairsInProgress } from "./MapRepairsInProgress";
 
 export const RepairsInProgress = () => {
-  const [repairs, setRepairs] = useState([])
-  const [clients, setClients] = useState([])
+  const [repairs, setRepairs] = useState([]);
+  const [clients, setClients] = useState([]);
+  console.log("usestate", clients);
 
   const getRepairs = () => {
-    const repairsCollection = collection(db, 'repairs')
+    const repairsCollection = collection(db, "repairs");
     const pendingRepairsQuery = query(
       repairsCollection,
-      where('isAccepted', '==', true),
-      where('isDone', '==', false),
-    )
+      where("isAccepted", "==", true),
+      where("isDone", "==", false)
+    );
 
-    getDocs(pendingRepairsQuery).then(QuerySnapshot => {
-      setRepairs(QuerySnapshot.docs.map(doc => ({ ...doc.data() })))
-    })
-  }
+    getDocs(pendingRepairsQuery).then((QuerySnapshot) => {
+      setRepairs(QuerySnapshot.docs.map((doc) => ({ ...doc.data() })));
+    });
+  };
 
   const getClients = async () => {
-    const clientsCollection = collection(db, 'clients')
-    const pendingClientsQuery = query(clientsCollection)
+    const clientsCollection = collection(db, "clients");
+    const pendingClientsQuery = query(clientsCollection);
 
-    const clientsQuerySnapshot = await getDocs(pendingClientsQuery)
-    const clientsDataPromises = clientsQuerySnapshot.docs.map(async doc => {
-      const { clientRepairs, ...clientData } = doc.data()
+    const clientsQuerySnapshot = await getDocs(pendingClientsQuery);
+    const clientsDataPromises = clientsQuerySnapshot.docs.map(async (doc) => {
+      const { clientRepairs, ...clientData } = doc.data();
       const clientRepairsData = await Promise.all(
-        clientRepairs.map(async repair => {
-          const repairData = await getDoc(repair)
-          return repairData.data()
-        }),
-      )
-      return { ...clientData, clientRepairs: clientRepairsData, id: doc.id }
-    })
-    const clientsData = await Promise.all(clientsDataPromises)
-    setClients(clientsData)
-    console.log(clientsData)
-  }
+        clientRepairs.map(async (repair) => {
+          const repairData = await getDoc(repair);
+          return repairData.data();
+        })
+      );
+      return { ...clientData, clientRepairs: clientRepairsData, id: doc.id };
+    });
+    const clientsData = await Promise.all(clientsDataPromises);
+    setClients(clientsData);
+    console.log(clientsData);
+  };
   useEffect(() => {
-    getRepairs()
-    getClients()
-  }, [])
+    getRepairs();
+    getClients();
+  }, []);
 
-  // console.log("rep", repairs);
-  // console.log("clie", clients);
   return (
     <>
       <MapRepairsInProgress repairs={repairs} clients={clients} />
     </>
-  )
-}
+  );
+};
