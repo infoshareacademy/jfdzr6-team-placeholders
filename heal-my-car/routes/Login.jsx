bcad03a4b8a25f09c843b69c559701823749d26b
Bartek Nowakowski <nowakowskib@gmail.com>
2022-06-25 19:54:56 +0200

    Creating NEW Login page-DRAFT

--- heal-my-car/routes/Login.jsx
+++ heal-my-car/routes/Login.jsx
@@ -1,6 +1,20 @@
 import React from 'react'
 import { useState } from 'react';
 import { Link, Navigate } from 'react-router-dom';
+// import * as React from 'react';
+// import Avatar from '@mui/material/Avatar';
+// import Button from '@mui/material/Button';
+// import CssBaseline from '@mui/material/CssBaseline';
+// import TextField from '@mui/material/TextField';
+// import FormControlLabel from '@mui/material/FormControlLabel';
+// import Checkbox from '@mui/material/Checkbox';
+// import Link from '@mui/material/Link';
+// import Grid from '@mui/material/Grid';
+// import Box from '@mui/material/Box';
+// import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+// import Typography from '@mui/material/Typography';
+// import Container from '@mui/material/Container';
+// import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 export const Login = () => {
     const [errorMessages, setErrorMessages] = useState({});
