ba17f968ddf334a18d268cb6cd60563f3aa5433c
Daniel <jackiewicz.daniel@gmail.com>
2022-06-26 13:06:40 +0200

    before PR

--- heal-my-car/components/admin-panel/SendRepairDone.jsx
+++ heal-my-car/components/admin-panel/SendRepairDone.jsx
@@ -4,23 +4,18 @@ import React from "react";
 import { db } from "../../src/Firebase";
 
 export const SendRepairDone = ({ clients, repair, getClients }) => {
-  const handleAccept = () => {
-    const taskRef = doc(db, "clients", "isDone");
+  console.log("clients", clients);
+  const handleAccept = (id, isDone) => {
+    const taskRef = doc(db, "clients");
     updateDoc(taskRef, {
       isDone: true,
     });
-    console.log("taskref", taskRef);
 
     getClients(clients);
   };
-  const id = clients.map((client) => {
-    return client.id;
-  });
-  console.log("client", id);
 
   return (
     <Button
-      key={id}
       onClick={() => {
         handleAccept();
       }}
