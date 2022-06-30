ca8245072be9883440cdfd59a78196ad68b36c69
Bartek Nowakowski <nowakowskib@gmail.com>
2022-06-30 22:14:06 +0200

    Repairstatus

--- heal-my-car/routes/ClientPanel.jsx
+++ heal-my-car/routes/ClientPanel.jsx
@@ -3,17 +3,18 @@ import { Outlet } from "react-router-dom";
 import ShowOffer from "../components/AcceptPricing/ShowOffer";
 import ClientPanelHeader from "./ClientPanelHeader";
 import { MapClientRepairs } from "../components/RapairClientStatus/MapClientRepairs";
+import { Repairstatus } from "../components/Repairstatus";
 
 export default function ClientPanel() {
   return (
     <>
       <ClientPanelHeader />
-      
-      <MapClientRepairs />
+      <ShowOffer />
+      <Repairstatus />
       <Outlet />
     </>
   );
 }
 
 
-//<ShowOffer />
+//<MapClientRepairs />
