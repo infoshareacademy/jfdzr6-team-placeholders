0b62e662d48bf5e4ed15d980fa36c6459fa3ec7d
Daniel <jackiewicz.daniel@gmail.com>
2022-07-02 07:34:58 +0200

    images paths corrected to dist

--- heal-my-car/components/LandingPage/AdminHeader.jsx
+++ heal-my-car/components/LandingPage/AdminHeader.jsx
@@ -97,7 +97,7 @@ const AdminHeader = () => {
       >
         <div className="landing-logo">
           <Link to="/" className="link">
-            <img src="../utils/logoGrad.png" height="75px" />
+            <img src="/logoGrad.png" height="75px" />
           </Link>
         </div>
 
